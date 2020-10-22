a6cca59bcdf8ccefd4b78c0e75993a9a02e90871
Jim Knowler <jim.knowler2@gmail.com>
2020-10-22 17:13:35 +0100

    VGA: relatively stable 800x600 red screen

--- 04_vga/top.v
+++ 04_vga/top.v
@@ -19,6 +19,10 @@ module top (
     // Basic VGA Output Circuit
     ////////
 
+    // references:
+    // - https://projectf.io/posts/video-timings-vga-720p-1080p/
+    // - http://martin.hinner.info/vga/timing.html
+
     // Generate a 40 MHz internal clock from 16 MHz input clock
     // - generated with icepll -16 -o 40
     wire clk_40;
@@ -39,19 +43,22 @@ module top (
 
     // generate VGA 800x600, 60Hz
     // taken from: http://martin.hinner.info/vga/timing.html
+
+    // horiz line
+    localparam H_BACK_PORCH = 88;
     localparam H_ACTIVE_VIDEO = 800;
     localparam H_FRONT_PORCH = 40;
     localparam H_SYNC_PULSE = 128;
-    localparam H_BACK_PORCH = 88;
+
+
     localparam V_ACTIVE_VIDEO = 600;
     localparam V_FRONT_PORCH = 1;
     localparam V_SYNC_PULSE = 4;
     localparam V_BACK_PORCH = 23;
 
     // intermediate calculations
-    localparam H_SYNC_PULSE_START = H_ACTIVE_VIDEO + H_FRONT_PORCH;
-    localparam H_SYNC_PULSE_END = H_SYNC_PULSE_START + H_SYNC_PULSE;
-    localparam H_END_MINUS_1 = H_SYNC_PULSE_END + H_BACK_PORCH - 1;
+    localparam H_SYNC_PULSE_START = H_BACK_PORCH + H_ACTIVE_VIDEO + H_FRONT_PORCH;
+    localparam H_SYNC_PULSE_END_MINUS_1 = H_SYNC_PULSE_START + H_SYNC_PULSE - 1;
 
     localparam V_SYNC_PULSE_START = V_ACTIVE_VIDEO + V_FRONT_PORCH;
     localparam V_SYNC_PULSE_END = V_SYNC_PULSE_START + V_SYNC_PULSE;
@@ -59,27 +66,27 @@ module top (
 
     // horizontal and vertical positions
     reg [10:0] h_counter;
-    reg [10:0] v_counter;
+    reg [9:0] v_counter;
 
     // signals for whether currently in active area of h / v
     wire is_h_active_video;
-    assign is_h_active_video = (h_counter < H_ACTIVE_VIDEO);
+    assign is_h_active_video = (h_counter >= H_BACK_PORCH) && (h_counter < (H_BACK_PORCH + H_ACTIVE_VIDEO));
 
     wire is_v_active_video;
     assign is_v_active_video = (v_counter < V_ACTIVE_VIDEO);
 
     // signals for whether sync is pulsing
     wire is_h_sync_pulse;
-    assign is_h_sync_pulse = (h_counter >= H_SYNC_PULSE_START) && (h_counter < H_SYNC_PULSE_END);
+    assign is_h_sync_pulse = (h_counter >= H_SYNC_PULSE_START);
 
     wire is_v_sync_pulse;
     assign is_v_sync_pulse = (v_counter >= V_SYNC_PULSE_START) && (v_counter < V_SYNC_PULSE_END);
 
     // x,y co-ords of current pixel to draw
-    wire [9:0] x;
+    wire [10:0] x;
     wire [9:0] y;
 
-    assign x[9:0] = is_h_active_video ? h_counter[9:0] : 0;
+    assign x[10:0] = is_h_active_video ? (h_counter[10:0] - H_BACK_PORCH) : 0;
     assign y[9:0] = is_v_active_video ? v_counter[9:0] : 0;
 
     wire is_active_video;
@@ -88,7 +95,7 @@ module top (
     // sequential logic for rendering
     always @(posedge clk_40) begin
       // beware of off-by-one errors when comparing to H_END / V_END
-      if (h_counter == H_END_MINUS_1) begin
+      if (h_counter == H_SYNC_PULSE_END_MINUS_1) begin
         h_counter <= 0;
 
         if (v_counter == V_END_MINUS_1) begin
@@ -110,9 +117,9 @@ module top (
     wire green;
     wire blue;
 
-    assign red = x < 400;
-    assign blue = y < 300;
-    assign green = x >= 400;
+    assign red = 1;   //x < 400;
+    assign blue = 0;  //y < 300;
+    assign green = 0; //x >= 400;
 
     assign PIN_11 = is_active_video && red;    // red
     assign PIN_12 = is_active_video && green;  // green
